4a26cc03c531ad58656537bbc1357f5b3217137e
Jonathan <jonathanafugwobi1@gmail.com>
2026-06-27 21:07:55 +0100

    Add Booking Page step 1 and 2 code logic

--- client/src/components/ui/Step2Service.jsx
+++ client/src/components/ui/Step2Service.jsx
@@ -1,10 +1,33 @@
+import services from "../../data/services";
 
-function Step2Service() {
+function Step2Service({ data, updateBooking }) {
+
+  const filteredServices = services.filter(
+    (service) => service.category === data?.category
+  );
+
   return (
     <div>
+      <div className="filtered-services-container">
+        {filteredServices.length > 0 ? (
+          filteredServices.map((service) => (
+            <div key={service.id} className="service-box">
+              <p>{service.name}</p>
+              <span>
+                <p>${service.price}</p>
+              </span>
 
+              <p>{service.description}</p>
+
+              <p>{service.time}</p>
+            </div>
+          ))
+        ) : (
+          <p>No service found in this category!</p>
+        )}
+      </div>
     </div>
-  )
+  );
 }
 
 export default Step2Service;
