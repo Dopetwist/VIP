7e1cc5d4cb903fb25422c96438c877f5252afc64
Jonathan <jonathanafugwobi1@gmail.com>
2026-07-05 13:16:28 +0100

    Add Header business logo. pass HomePage 'ServiceSection' clicked service to 'ServicePage' for complete services display

---
+++ client/src/components/animation/Toast.jsx
@@ -0,0 +1,23 @@
+import { useEffect } from "react";
+
+function Toast({ message, type = "success", onClose }) {
+
+    useEffect(() => {
+        const timer = setTimeout(() => {
+            onClose();
+        }, 3000); // auto close after 3s
+
+        // clean-up function
+        return () => clearTimeout(timer);
+    }, [onClose]);
+
+    return (
+        <div className="toast-container">
+            <div className={`toast ${type}`}>
+                <p id="toast-text">{message}</p>
+            </div>
+        </div>
+    );
+}
+
+export default Toast;
