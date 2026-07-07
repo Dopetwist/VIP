476aff0bf2c46e182c3dfa5d9676e0addba4be15
Jonathan <jonathanafugwobi1@gmail.com>
2026-07-07 21:54:30 +0100

    Add Toast for Dashboard Appointment component buttons

--- client/src/pages/Dashboard.jsx
+++ client/src/pages/Dashboard.jsx
@@ -1,10 +1,12 @@
 import { useState } from "react";
 import { useLocation } from "react-router";
+import { CircleAlert } from "lucide-react";
 import UserAccount from "../components/ui/dashboard/UserAccount";
 import DashboardOverview from "../components/ui/dashboard/DashboardOverview";
 import Appointments from "../components/ui/dashboard/Appointments";
 import Orders from "../components/ui/dashboard/Orders";
 import Profile from "../components/ui/dashboard/Profile";
+import Toast from "../components/animation/Toast";
 
 export default function Dashboard() {
     const [activeView, setActiveView] = useState("overview");
@@ -13,10 +15,18 @@ export default function Dashboard() {
 
     const serviceData = location?.state?.data;
 
+    const [ toast, setToast ] = useState(null);
+    
+    const handleToast = (text) => {
+        setToast({
+            message: <span className="circle-check"><CircleAlert className="circle-icon" /> {text} feature coming soon! </span>
+        })
+    }
+
     const renderPanel = () => {
         switch (activeView) {
             case "appointments":
-                return <Appointments data={serviceData} />;
+                return <Appointments data={serviceData} handleToast={handleToast} />;
             case "orders":
                 return <Orders />;
             case "profile":
@@ -35,6 +45,14 @@ export default function Dashboard() {
                 <UserAccount activeView={activeView} onNavigate={setActiveView} />
                 <div className="dashboard-content-panel">{renderPanel()}</div>
             </div>
+
+            {/* Render Toast */}
+            {toast && (
+                <Toast
+                    message={toast.message}
+                    onClose={() => setToast(null)}
+                />
+            )}
         </div>
     );
 }
